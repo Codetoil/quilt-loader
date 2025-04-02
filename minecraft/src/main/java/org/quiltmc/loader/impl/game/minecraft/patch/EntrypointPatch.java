645d6bfe3116259db71b46ba5dd45afb66bbb84b
VelizarBG <31567122+VelizarBG@users.noreply.github.com>
2025-04-02 15:44:11 +0300

    Add support for 25w14craftmine dedicated servers (#1030)

--- minecraft/src/main/java/org/quiltmc/loader/impl/game/minecraft/patch/EntrypointPatch.java
+++ minecraft/src/main/java/org/quiltmc/loader/impl/game/minecraft/patch/EntrypointPatch.java
@@ -53,6 +53,7 @@ import org.quiltmc.loader.impl.game.minecraft.Hooks;
 public class EntrypointPatch extends GamePatch {
 	/* QUILT */private static final VersionPredicate BEFORE_1_17 = createVersionPredicate("<=1.17");
 	private static final VersionPredicate VERSION_1_19_4 = createVersionPredicate(">=1.19.4-");
+	private static final VersionPredicate VERSION_25w14craftmine = createVersionPredicate("1.21.6-alpha.25.14.craftmine");
 
 	private final MinecraftGameProvider gameProvider;
 
@@ -320,7 +321,8 @@ public class EntrypointPatch extends GamePatch {
 				// If we do not find this, then we are certain this is 20w22a.
 				MethodNode serverStartMethod = findMethod(mainClass, method -> {
 					if ((method.access & Opcodes.ACC_SYNTHETIC) == 0 // reject non-synthetic
-							|| method.name.equals("main") && method.desc.equals("([Ljava/lang/String;)V")) { // reject main method (theoretically superfluous now)
+							|| method.name.equals("main") && method.desc.equals("([Ljava/lang/String;)V") // reject main method (theoretically superfluous now)
+							|| VERSION_25w14craftmine.test(gameVersion) && method.parameters.size() < 10) { // reject problematic extra methods
 						return false;
 					}
 
